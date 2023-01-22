e992ffbb002324f9a44bca23d4f269869b85c54a
Olivier Anguenot <oanguenot@gmail.com>
2023-01-22 13:56:50 +0100

    Adapt existing sample for editor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import Drawflow from "drawflow";
+
+import "./App.css";
+import "./resources/beautify.css";
+import { useEffect, useRef } from "react";
+
+let editor = null;
 
 function App() {
+  const drawFlowElt = useRef(null);
+  const lock = useRef(null);
+  const unlock = useRef(null);
+
+  useEffect(() => {
+    editor = new Drawflow(drawFlowElt.current);
+    editor.start();
+  }, []);
+
+  const allowDrop = (event) => {
+    event.preventDefault();
+  };
+
+  const onDrop = () => {};
+
+  const onClear = () => {
+    editor.clearModuleSelected();
+  };
+
+  const onLock = () => {
+    editor.editor_mode = "fixed";
+    changeMode("lock");
+  };
+
+  const onUnlock = () => {
+    editor.editor_mode = "edit";
+    changeMode("unlock");
+  };
+
+  const onZoomIn = () => {
+    editor.zoom_in();
+  };
+
+  const onZoomOut = () => {
+    editor.zoom_out();
+  };
+
+  const onZoomReset = () => {
+    editor.zoom_reset();
+  };
+
+  const changeMode = (option) => {
+    if (option === "lock") {
+      lock["style"].display = "none";
+      unlock["style"].display = "block";
+    } else {
+      lock["style"].display = "block";
+      unlock["style"].display = "none";
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <div className="col"></div>
+        <div className="col-right">
+          <div className="menu">
+            <ul>
+              <li className="selected">Home</li>
+            </ul>
+          </div>
+          <div
+            id="drawflow"
+            ref={drawFlowElt}
+            onDrop={(event) => onDrop(event)}
+            onDragOver={(event) => allowDrop(event)}
+          ></div>
+          <div className="btn-export">Export</div>
+          <div className="btn-clear" onClick={() => onClear()}>
+            Clear
+          </div>
+          <div className="btn-lock">
+            <i
+              id="lock"
+              ref={lock}
+              className="fas fa-lock"
+              onClick={() => {
+                onLock();
+              }}
+            ></i>
+            <i
+              id="unlock"
+              ref={unlock}
+              className="fas fa-lock-open"
+              onClick={() => {
+                onUnlock();
+              }}
+              style={{ display: "none" }}
+            ></i>
+          </div>
+          <div className="bar-zoom">
+            <i className="fas fa-search-minus" onClick={() => onZoomOut()}></i>
+            <i className="fas fa-search" onClick={() => onZoomReset()}></i>
+            <i className="fas fa-search-plus" onClick={() => onZoomIn()}></i>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
