05e4fae7c32b4171f7431ec4ee16c6cbf0cf9a7f
Olivier Anguenot <olivier.anguenot@gmail.com>
2023-02-12 09:42:46 +0100

    Create peer connection and media inside iframe

--- src/modules/playground.js
+++ src/modules/playground.js
@@ -1,3 +1,5 @@
+const frames = {};
+
 const createIFrame = (peerNode) => {
   const iframesElt = document.querySelector("#frames");
   const iframe = document.createElement('iframe');
@@ -5,23 +7,47 @@ const createIFrame = (peerNode) => {
   iframe.setAttribute("id", peerNode.id);
   iframe.src = "./iframe.html";
   iframesElt.appendChild(iframe);
+
+  return iframe.contentWindow;
 }
 
 
 const createPeerConnection = (peerNode) => {
+  return new Promise((resolve, reject) => {
+    const win = frames[peerNode.id];
 
+    if(win) {
+      const pc = new win.RTCPeerConnection();
+    }
+    resolve();
+  });
+}
+
+const createMedia = (peerNode) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const win = frames[peerNode.id];
+      const stream = await win.navigator.mediaDevices.getUserMedia({audio: true, video: true});
+      resolve(stream);
+    } catch(err) {
+      reject(err);
+    }
+  });
 }
 
 
 export const execute = (nodes) => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     // found peer connections for creating iFrame
     const peers = nodes.filter(item => (item.node === "rtc.peer"));
-    peers.forEach(peer => {
-      createIFrame(peer);
-      createPeerConnection(peer);
-    });
 
+    for(const peer of peers) {
+      const win = createIFrame(peer);
+      // Store iframe window context associated to a peer connection
+      frames[peer.id] = win;
+      await createPeerConnection(peer);
+      const stream = await createMedia(peer);
+    }
     resolve();
   });
 }
