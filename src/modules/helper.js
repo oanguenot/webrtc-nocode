83f32a4f5f7dcdac90dfc5e50026f3ac74ac8d8f
Olivier Anguenot <oanguenot@gmail.com>
2023-02-19 21:16:56 +0100

    First try to add listener on ice connection change

--- src/modules/helper.js
+++ src/modules/helper.js
@@ -1,44 +1,55 @@
-import {KEYS, NODES} from "./model";
+import { KEYS, NODES } from "./model";
 
-export const getDimensionFromResolution = (resolution)  =>{
+export const getDimensionFromResolution = (resolution) => {
   switch (resolution) {
     case "480p":
-      return {width: {ideal: 640}, height: {ideal: 480}};
+      return { width: { ideal: 640 }, height: { ideal: 480 } };
     case "720p":
-      return {width: {ideal: 1280}, height: {ideal: 720}};
+      return { width: { ideal: 1280 }, height: { ideal: 720 } };
     case "1080p":
-      return {width: {ideal: 1920}, height: {ideal: 1080}};
+      return { width: { ideal: 1920 }, height: { ideal: 1080 } };
     case "4k":
-      return {width: {ideal: 3840}, height: {ideal: 2160}};
+      return { width: { ideal: 3840 }, height: { ideal: 2160 } };
   }
-}
+};
 
 export const includes = (nodeName, list) => {
   let found = false;
-  list.forEach(item => {
-    if(item === nodeName || nodeName.includes(item)) {
+  list.forEach((item) => {
+    if (item === nodeName || nodeName.includes(item)) {
       found = true;
     }
   });
   return found;
-}
+};
 
-export const getPeers = (nodes) => (nodes.filter(item => (item.node === NODES.PEER)));
+export const getPeers = (nodes) =>
+  nodes.filter((item) => item.node === NODES.PEER);
 
-export const getReady = (nodes) => (nodes.find(item => (item.node === NODES.READY)));
+export const getReady = (nodes) =>
+  nodes.find((item) => item.node === NODES.READY);
 
-export const getIceChange = (nodes) => (nodes.filter(item => (item.node === NODES.ICE)));
+export const getIceChange = (nodes) =>
+  nodes.filter((item) => item.node === NODES.ICE);
 
-export const getNodeById = (id, nodes) => (nodes.find((node) => (node.id === id)));
+export const getNodeById = (id, nodes) => nodes.find((node) => node.id === id);
 
-export const getNodeIndexById = (id, nodes) => (nodes.findIndex((object) => (object.id === id)));
+export const getNodeIndexById = (id, nodes) =>
+  nodes.findIndex((object) => object.id === id);
 
 export const getNodeInfoValue = (infoKey, node) => {
   const info = node._info;
-  if(!info) {
+  if (!info) {
     return null;
   }
 
-  const infoName = info.find(item => item.key === infoKey);
+  const infoName = info.find((item) => item.key === infoKey);
   return infoName ? infoName.value : null;
-}
+};
+
+export const filterICENodeForPeer = (ICENodes, peerNodeId) => {
+  return ICENodes.filter((node) => {
+    const peerId = node.getPropertyValueFor(KEYS.PEER);
+    return peerId === peerNodeId;
+  });
+};
--- src/modules/playground.js
+++ src/modules/playground.js
@@ -1,21 +1,30 @@
-import {getDimensionFromResolution, getIceChange, getNodeById, getNodeInfoValue, getPeers, getReady} from "./helper";
-import {KEYS, NODES} from "./model";
-import {rehydrateObject} from "./builder";
+import {
+  filterICENodeForPeer,
+  getDimensionFromResolution,
+  getIceChange,
+  getNodeById,
+  getNodeInfoValue,
+  getPeers,
+  getReady,
+} from "./helper";
+import { KEYS, NODES } from "./model";
+import { rehydrateObject } from "./builder";
 
 const frames = {};
 
 const delayer = (duration) => {
   return new Promise((resolve, __reject) => {
+    console.log(`${new Date().toJSON()} [play] execute delay of ${duration}ms`);
     setTimeout(() => {
       resolve();
     }, duration);
   });
-}
+};
 
 const createIFrame = (peerNode) => {
   return new Promise((resolve, reject) => {
     const iframesElt = document.querySelector("#frames");
-    const iframe = document.createElement('iframe');
+    const iframe = document.createElement("iframe");
     iframe.addEventListener("load", () => {
       resolve(iframe.contentWindow);
     });
@@ -23,59 +32,70 @@ const createIFrame = (peerNode) => {
     iframe.src = `${window.location.href}/iframe.html`;
     iframesElt.appendChild(iframe);
   });
-}
+};
 
 const updateTitleInIFrame = (win, id) => {
   const titleElt = win.document.querySelector("#frameTitle");
   if (titleElt) {
     titleElt.innerHTML = id;
   }
-}
+};
 
-const addDefaultMediaInIFrame = (win, kind, id, isLocal=true) => {
-  const localElt = win.document.querySelector(`${isLocal ? "#local" : "#remote"}`);
+const addDefaultMediaInIFrame = (win, kind, id, isLocal = true) => {
+  const localElt = win.document.querySelector(
+    `${isLocal ? "#local" : "#remote"}`
+  );
   const elt = win.document.createElement(kind);
   elt.setAttribute("id", `${isLocal ? "local" : "remote"}-${id}`);
   elt.setAttribute("width", "64");
   elt.setAttribute("height", "64");
   elt.setAttribute("playsinline", null);
   elt.setAttribute("autoplay", null);
-  if(isLocal) {
+  if (isLocal) {
     elt.setAttribute("muted", null);
   }
   localElt.appendChild(elt);
-}
+};
 
-const createPeerConnection = (peerNode, stream, iceEvents) => {
+const createPeerConnection = (peerNode, stream, iceEvents, nodes) => {
   return new Promise((resolve, reject) => {
     const win = frames[peerNode.id];
 
-    if(win) {
+    if (win) {
       win.pc = new win.RTCPeerConnection();
       win.ices = [];
       win.pc.oniceconnectionstatechange = () => {
-        console.log(`[ice] ${peerNode.id} state changed to ${win.pc.iceConnectionState}`)
+        const state = win.pc.iceConnectionState;
+        console.log(`[ice] ${peerNode.id} state changed to ${state}`);
         // Check iceEvents node to initiate actions
-      }
+        iceEvents.forEach((eventNode) => {
+          const eventState = eventNode.getPropertyValueFor(KEYS.ICESTATE);
+          if (eventState === state) {
+            console.log(`[ice] execute node ${eventNode.id}`);
+            executeEventNode(eventNode, nodes);
+          }
+        });
+      };
       win.pc.onicecandidate = (event) => {
         win.ices.push(event.candidate);
-      }
+      };
       win.pc.ontrack = (event) => {
         console.log(`[track] ${peerNode.id} received`, event.track);
-        addDefaultMediaInIFrame(win, event.track.kind, event.track.id,false);
+        addDefaultMediaInIFrame(win, event.track.kind, event.track.id, false);
         const captured = new win.MediaStream();
         captured.addTrack(event.track);
-        win.document.querySelector(`#remote-${event.track.id}`).srcObject = captured;
-      }
+        win.document.querySelector(`#remote-${event.track.id}`).srcObject =
+          captured;
+      };
 
-      stream.getTracks().forEach(track => {
+      stream.getTracks().forEach((track) => {
         console.log(`[track] ${peerNode.id} add`, track);
         win.pc.addTrack(track);
       });
     }
     resolve();
   });
-}
+};
 
 const createMedia = (peerNode, nodes) => {
   return new Promise(async (resolve, reject) => {
@@ -84,17 +104,17 @@ const createMedia = (peerNode, nodes) => {
       win.stream = new win.MediaStream();
 
       for (const inputId of peerNode.linksInput) {
-        const constraints = {audio: false, video: false};
+        const constraints = { audio: false, video: false };
         let input = getNodeById(inputId, nodes);
         if (input) {
-          const kind = input.getInfoValueFor('kind');
+          const kind = input.getInfoValueFor("kind");
           const deviceId = input.getPropertyValueFor("from");
           if (kind === "audio") {
             const channelCount = input.getPropertyValueFor("channelCount");
             constraints.audio = {
               channelCount,
-              deviceId
-            }
+              deviceId,
+            };
           } else {
             const framerate = input.getPropertyValueFor("framerate");
             const resolution = input.getPropertyValueFor("resolution");
@@ -103,85 +123,94 @@ const createMedia = (peerNode, nodes) => {
               framerate,
               deviceId,
               width: dimension.width,
-              height: dimension.height
-            }
+              height: dimension.height,
+            };
           }
           // Create media element in IFrame
           addDefaultMediaInIFrame(win, kind, deviceId);
-          const captured = await win.navigator.mediaDevices.getUserMedia(constraints);
+          const captured = await win.navigator.mediaDevices.getUserMedia(
+            constraints
+          );
           win.document.querySelector(`#local-${deviceId}`).srcObject = captured;
-          captured.getTracks().forEach(track => win.stream.addTrack(track));
+          captured.getTracks().forEach((track) => win.stream.addTrack(track));
         }
       }
       resolve(win.stream);
-    } catch(err) {
+    } catch (err) {
       console.log(">>>Reject", err);
       reject(err);
     }
   });
-}
+};
 
 const call = (callerNode, calleeNode, callNode) => {
   return new Promise(async (resolve, reject) => {
     const callerWin = frames[callerNode.id];
     const calleeWin = frames[calleeNode.id];
-    if(!callerWin || !calleeWin || !callerWin.pc || !calleeWin.pc) {
+    if (!callerWin || !calleeWin || !callerWin.pc || !calleeWin.pc) {
       console.warn("Can't call - can't find frames or pc");
       reject();
     }
 
+    console.log(`${new Date().toJSON()} >>>execute call`);
     const offer = await callerWin.pc.createOffer();
     await callerWin.pc.setLocalDescription(offer);
     await delayer(2000);
     await calleeWin.pc.setRemoteDescription(offer);
 
-    callerWin.ices.forEach(ice => calleeWin.pc.addIceCandidate(ice));
+    callerWin.ices.forEach((ice) => calleeWin.pc.addIceCandidate(ice));
     const answer = await calleeWin.pc.createAnswer();
     await calleeWin.pc.setLocalDescription(answer);
     await delayer(2000);
     await callerWin.pc.setRemoteDescription(answer);
-    calleeWin.ices.forEach(ice => callerWin.pc.addIceCandidate(ice));
+    calleeWin.ices.forEach((ice) => callerWin.pc.addIceCandidate(ice));
 
+    console.log(`${new Date().toJSON()} >>> terminate call`);
     await delayer(2000);
-    if(callerWin.pc) {
+    if (callerWin.pc) {
       callerWin.pc.close();
     }
-    if( calleeWin.pc) {
+    if (calleeWin.pc) {
       calleeWin.pc.close();
     }
-    if(callerWin.stream) {
-      callerWin.stream.getTracks().forEach(track => track.stop());
+    if (callerWin.stream) {
+      callerWin.stream.getTracks().forEach((track) => track.stop());
     }
-    if(calleeWin.stream) {
-      calleeWin.stream.getTracks().forEach(track => track.stop());
+    if (calleeWin.stream) {
+      calleeWin.stream.getTracks().forEach((track) => track.stop());
     }
     resolve();
   });
-}
+};
 
-const executeReadyStep = (readyNode, nodes) => {
-  return new Promise( (resolve, reject) => {
-
-    const firstNode = getNodeById(readyNode.linksOutput[0], nodes);
-    if(!firstNode) {
+const executeEventNode = (eventNode, nodes) => {
+  return new Promise((resolve, reject) => {
+    const firstNode = getNodeById(eventNode.linksOutput[0], nodes);
+    if (!firstNode) {
       resolve();
       return;
     }
 
-    executeANode(readyNode, firstNode, nodes).then(() => {
+    executeANode(eventNode, firstNode, nodes).then(() => {
       resolve();
-    })
+    });
   });
-}
+};
 
 const executeANode = (initialEvent, currentNode, nodes) => {
   console.log("[play] execute node", currentNode.id);
-  return new Promise( (resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const promises = [];
     switch (currentNode.node) {
       case NODES.CALL:
-        const fromPeer = getNodeById(initialEvent.getPropertyValueFor("peer"), nodes);
-        const recipientPeer = getNodeById(currentNode.getPropertyValueFor("peer"), nodes);
+        const fromPeer = getNodeById(
+          initialEvent.getPropertyValueFor("peer"),
+          nodes
+        );
+        const recipientPeer = getNodeById(
+          currentNode.getPropertyValueFor("peer"),
+          nodes
+        );
         if (recipientPeer && fromPeer) {
           promises.push(call(fromPeer, recipientPeer, currentNode));
         } else {
@@ -189,13 +218,19 @@ const executeANode = (initialEvent, currentNode, nodes) => {
           reject();
         }
         break;
+      case NODES.WAIT:
+        const delay = currentNode.getPropertyValueFor(KEYS.DELAY);
+        promises.push(delayer(delay));
+      case NODES.END:
+        console.log("[play] end reached!!!");
+        promises.push(Promise.resolve());
       default:
         break;
     }
 
     Promise.all(promises).then(() => {
       const nextNode = getNodeById(currentNode.linksOutput[0], nodes);
-      if(!nextNode) {
+      if (!nextNode) {
         console.log("[play] no more step in ready");
         resolve();
         return;
@@ -205,7 +240,7 @@ const executeANode = (initialEvent, currentNode, nodes) => {
       return executeANode(initialEvent, nextNode, nodes);
     });
   });
-}
+};
 
 export const execute = (nodes) => {
   return new Promise(async (resolve, reject) => {
@@ -215,31 +250,31 @@ export const execute = (nodes) => {
     const iceEvents = getIceChange(nodes);
 
     // Initialize Peer Connections
-    for(const peer of peers) {
+    for (const peer of peers) {
       const win = await createIFrame(peer);
       updateTitleInIFrame(win, peer.id);
       // Store iframe window context associated to a peer connection
       frames[peer.id] = win;
       const stream = await createMedia(peer, nodes);
-      await createPeerConnection(peer, stream, iceEvents);
-
+      const iceEventsForPeer = filterICENodeForPeer(iceEvents, peer.id);
+      await createPeerConnection(peer, stream, iceEventsForPeer, nodes);
     }
 
     // Check for the ready event and execute it
     const ready = getReady(nodes);
-    if(!ready) {
+    if (!ready) {
       reject("No ready event");
     }
-    executeReadyStep(ready, nodes).then(() => {
+    executeEventNode(ready, nodes).then(() => {
       console.log("[play] ended...");
       resolve();
     });
   });
-}
+};
 
 export const rehydrateModel = (nodes) => {
   const model = [];
-  nodes.forEach(node => {
+  nodes.forEach((node) => {
     const name = getNodeInfoValue(KEYS.NODE, node);
     const kind = getNodeInfoValue(KEYS.KIND, node);
     const object = rehydrateObject(name, kind, node._posX, node._posY);
@@ -247,14 +282,14 @@ export const rehydrateModel = (nodes) => {
     model.push(object);
   });
   return model;
-}
+};
 
 export const rehydrateDOM = (nodes) => {
   // update DOM with current properties stored
-  nodes.forEach(node => {
+  nodes.forEach((node) => {
     const properties = node.properties;
-    properties.forEach(property => {
+    properties.forEach((property) => {
       node.updateDisplayInObject(property.prop);
     });
   });
-}
+};
